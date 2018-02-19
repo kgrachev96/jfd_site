ffcac4861e9701318dba7a5b29c896563f239abc
Kirill Grachev <kgrachev96@gmail.com>
2018-02-19 18:31:04 +0300

    update modal form

--- src/components/FormDialog/index.tsx
+++ src/components/FormDialog/index.tsx
@@ -5,7 +5,6 @@ import { Col, ControlLabel, Form, FormControl, FormGroup, Modal } from "react-bo
 import * as ReactDOM from "react-dom";
 
 import Button from "../Button";
-import FormInput from "../FormInput";
 
 import "./style.scss";
 
@@ -13,8 +12,49 @@ export default class FormDialog extends React.Component<any, any> {
     constructor(props: any) {
         super(props);
 
+        this.state = {
+            name: "",
+            email: "",
+            valid: true,
+            emailErrorField: "",
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.nameChange = this.nameChange.bind(this);
+        this.emailChange = this.emailChange.bind(this);
+
     }
+
+    public validateEmail(email: any) {
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
+
+    public handleSubmit(event: any) {
+        event.preventDefault();
+        const name = this.props.name;
+    }
+
+    public nameChange(event: any) {
+        const name = event.target.value;
+        this.setState({ name });
+    }
+
+    public emailChange(event: any) {
+        const email = event.target.value;
+        const emailValid = this.validateEmail(email);
+        this.setState({
+            email: event.target.value,
+            valid: emailValid,
+        });
+    }
+
     public render() {
+        let emailErrorField = "";
+        const { email, valid } = this.state;
+
+        (!valid) ? emailErrorField += "Введите корректный e-mail" : (email === "" ? emailErrorField = "" : "");
+
         return (
             <Modal
                 show={this.props.show}
@@ -27,13 +67,39 @@ export default class FormDialog extends React.Component<any, any> {
                     <h2>Моё портфолио Вас заинтересовало? Напишите мне. Я буду очень рад Вашему письму!</h2>
                     <div>
                         <MuiThemeProvider>
-                            <FormInput />
+                            <div id="contact-form">
+                                <div className="contact-inputs">
+                                    <TextField
+                                        className="firstname"
+                                        value={this.state.name}
+                                        floatingLabelText="Поле для ввода имени..."
+                                        type="text"
+                                        onChange={this.nameChange}
+                                    />
+
+                                    <TextField
+                                        className="email"
+                                        errorText={emailErrorField}
+                                        value={this.state.email}
+                                        floatingLabelText="Поле для ввода email..."
+                                        type="email"
+                                        onChange={this.emailChange}
+                                    />
+                                </div>
+                                <TextField
+                                    className="message"
+                                    floatingLabelText="Поле для ввода сообщения..."
+                                    multiLine={true}
+                                    rows={8}
+                                    fullWidth={true}
+                                />
+                            </div>
                         </MuiThemeProvider>
                     </div>
 
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button className="btn btn-modal success" type="submit">Отправить письмо</Button>
+                    <Button className="btn btn-modal success" onClick={this.props.send} type="submit">Отправить письмо</Button>
                     <Button className="btn btn-modal exit" onClick={this.props.hide}>Закрыть</Button>
                 </Modal.Footer>
             </Modal >
